a7815e98547655159b3895b5c2cb526918ad90f1
nurfiiitriiiyah <nurfiitriiyah@gmail.com>
2019-05-21 09:04:42 +0700

    CALL HALPER JWT

--- controllers/controller_ots.go
+++ controllers/controller_ots.go
@@ -2,45 +2,49 @@ package controllers
 
 import (
 	"../structs"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 // to get one data with {id}
 func (idb *InDB) GetOTS(c *gin.Context) {
-	/*
-		Get OTS
-	*/
+	/*Get OTS*/
 	var (
 		Ots    structs.TbOutstanding
 		result gin.H
 	)
 	areaOts := make(map[string][]interface{})
+	dispOts := make(map[string][]interface{})
 
-	rows, err := idb.DB.Table("tb_outstandings").Select("outstanding_area,sum(outstanding_quantity)").Group("outstanding_dispatcher").Rows()
+	rows, err := idb.DB.Table("tb_outstandings").Select("outstanding_area,sum(outstanding_quantity)").Group("outstanding_area").Rows()
+	disp, err := idb.DB.Table("tb_outstandings").Select("outstanding_dispatcher,sum(outstanding_quantity)").Group("outstanding_dispatcher").Rows()
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, err)
 		c.Abort()
 	} else {
-		i := 1
+		for disp.Next() {
+			err := disp.Scan(&Ots.Outstanding_dispatcher, &Ots.Outstanding_quantity)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, err)
+				c.Abort()
+			} else {
+				dispOts[Ots.Outstanding_dispatcher] = append(areaOts[Ots.Outstanding_dispatcher], Ots.Outstanding_quantity)
+			}
+		}
 		for rows.Next() {
 			err := rows.Scan(&Ots.Outstanding_area, &Ots.Outstanding_quantity)
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, err)
 				c.Abort()
 			} else {
-				areaOts[Ots.Outstanding_area] = append(areaOts[Ots.Outstanding_area], Ots.Outstanding_area, Ots.Outstanding_quantity)
-				fmt.Println(Ots)
+				areaOts[Ots.Outstanding_area] = append(areaOts[Ots.Outstanding_area], Ots.Outstanding_quantity)
 			}
-			i++
 		}
-		fmt.Println(areaOts)
-
 	}
 	result = gin.H{
 		"area": areaOts,
+		"disp": dispOts,
 	}
 
 	c.JSON(http.StatusOK, result)
